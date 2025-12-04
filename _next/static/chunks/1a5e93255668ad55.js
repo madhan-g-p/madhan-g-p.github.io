57e11e716894538ecb9c375c4b4fe8d057f9fa3b
Madhana Gopal <madhanag04@gmail.com>
2025-12-04 22:23:59 +0530

    css updates

---
+++ _next/static/chunks/1a5e93255668ad55.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/d088e6ef5b0b2c22.js",
+  "static/chunks/1238165cc8e5cba0.js",
+  "static/chunks/7e02ef6bc2c76bb7.js",
+  "static/chunks/c9786ab30350381c.css",
+  "static/chunks/turbopack-d5d6ca32b5336b21.js"
+])
---
+++ _next/static/chunks/8aa2962e64211835.js
@@ -0,0 +1,7 @@
+__turbopack_load_page_chunks__("/", [
+  "static/chunks/8569bba790448ed1.js",
+  "static/chunks/4c32557e32ae4612.js",
+  "static/chunks/7e02ef6bc2c76bb7.js",
+  "static/chunks/1238165cc8e5cba0.js",
+  "static/chunks/turbopack-d3090481aed3d6bb.js"
+])
